6b85e7751d3e3094ef355d325d0d33e657a56f3d
Al <Sasha252VNS@gmail.com>
2021-06-01 01:35:44 +0300

    1

--- trl/gpt2.py
+++ trl/gpt2.py
@@ -142,11 +142,12 @@ def respond_to_batch(model, queries, mask=None, seq_ids=None,
         probs = torch.softmax(next_token_logits, dim=-1)
         next_token = torch.multinomial(probs, num_samples=1)
 
-        generation_finished = torch.where(next_token == bos_token, ones, generation_finished)
-        input_seq = torch.cat([input_seq, next_token], dim=-1)
         mask = torch.cat([mask, (1 - generation_finished).long()], dim=-1)
-        new_ids = (seq_ids[:, -1:] + (1 - generation_finished).long())
-        seq_ids = torch.cat([seq_ids, new_ids], dim=-1)
+        input_seq = torch.cat([input_seq, next_token], dim=-1)
+        generation_finished = torch.where(next_token == bos_token, ones, generation_finished)
+        #new_ids = (seq_ids[:, -1:] + (1 - generation_finished).long())
+        #seq_ids = torch.cat([seq_ids, new_ids], dim=-1)
+        seq_ids = torch.cumsum(mask, -1) - 1
 
         if torch.all(generation_finished == 1):
             input_seq, mask, seq_ids = pad_seqs(input_seq, mask, seq_ids, txt_len - i - 1, pad_token)
